5592949c9bce78bf189bf0fb617aa3d45cf722d7
vinicius <viniciush4@gmailcom>
2018-05-27 12:48:21 -0300

    Criação da função imprimirEntradas()

--- src/Main.java
+++ src/Main.java
@@ -22,14 +22,56 @@ public class Main
 	public static void main(String[] args) 
 	{
 		lerEntradas();
+		imprimirEntradas();
 		analisaEntradas();
 		imprimirTableau();
 	}
 	
+	private static void lerEntradas()
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		tipoProblema = scanner.nextLine();
+		String dimensoes[] = scanner.nextLine().split(" ");
+		quantidadeRestricoes = Integer.parseInt(dimensoes[0]);
+		quantidadeVariaveisNaturais = Integer.parseInt(dimensoes[1]);
+		restricoes = new String[quantidadeRestricoes][quantidadeVariaveisNaturais+2];
+		funcaoObjetivo = new float[quantidadeVariaveisNaturais];
+		
+		for(int i = 0; i < quantidadeVariaveisNaturais; i++) {
+			funcaoObjetivo[i] = scanner.nextFloat();
+		}
+		scanner.nextLine();
+		
+		for(int i = 0; i < quantidadeRestricoes; i++) {
+			restricoes[i] = scanner.nextLine().split(" ");
+		}
+		scanner.close();
+	}
 	
+	private static void imprimirEntradas()
+	{
+		System.err.println(tipoProblema + " \n" + quantidadeRestricoes + " " + quantidadeVariaveisNaturais);
+		
+		for(int i = 0; i < quantidadeVariaveisNaturais; i++) {
+			System.out.print(funcaoObjetivo[i]+"   ");
+		}
+		System.out.println();
+	}
 	
-	//A ordem no tableau vai ser variaveis de folga, de excesso e artificiais
-	private static void analisaEntradas() {
+	private static void imprimirTableau()
+	{
+		for(int i = 0; i < numeroDeLinhasTableau; i++) {
+			for(int j = 0; j < numeroDeColunasTableau; j++) {
+				System.out.print(tableau[i][j]+"\t");
+			}
+			System.out.println();
+		}		
+	}
+	
+	// A ordem no tableau vai ser variaveis de folga, de excesso e artificiais
+	private static void analisaEntradas() 
+	{
 		for(int i = 0; i < quantidadeRestricoes; i++) {
 			if(restricoes[i][quantidadeVariaveisNaturais].equals("<=")) {
 				variaveisDeFolga++;
@@ -49,17 +91,8 @@ public class Main
 		}else {
 			numeroDeLinhasTableau = quantidadeRestricoes+2;
 			tableauComArtificial();
-		}
-			
-		
-		
-		
-		
-		
-		
+		}	
 	}
-		
-		
 	
 	private static void tableauComArtificial() {
 		tableau = new Float[numeroDeLinhasTableau][numeroDeColunasTableau];
@@ -69,7 +102,6 @@ public class Main
 			}
 		}
 		
-		
 		for(int i = 0; i < quantidadeVariaveisNaturais; i++) {
 			if(tipoProblema.equals("max")) {
 				tableau[1][i+1] = funcaoObjetivo[i];
@@ -78,12 +110,10 @@ public class Main
 				tableau[1][i+1] = funcaoObjetivo[i]*(-1);
 		}
 		
-		
 		for(int i = 0; i < variaveisArtificiais; i ++) {
 			System.out.println(i);
 			tableau[0][quantidadeVariaveisNaturais+variaveisDeExcesso+variaveisDeFolga+1+i] = (float) -1;
 		}
-		
 		
 		for(int i = 0; i < restricoes.length; i++) {
 			for(int j = 0; j < restricoes[0].length-2; j++) {
@@ -91,13 +121,10 @@ public class Main
 			}	
 		}
 		
-		
 		for(int i = 0; i < quantidadeRestricoes; i++) {
 			tableau[i+2][quantidadeVariaveisNaturais] = Float.valueOf(restricoes[i][quantidadeVariaveisNaturais-1]);
 			tableau[i+2][0] = Float.valueOf(restricoes[i][quantidadeVariaveisNaturais+1]);
 		}
-			
-		
 		
 		int posicaoDaFolga = quantidadeVariaveisNaturais+1;
 		int posicaoDaArtificial = numeroDeColunasTableau - variaveisArtificiais;
@@ -112,10 +139,7 @@ public class Main
 				tableau[i+2][posicaoDaArtificial++] = (float) 1;
 			}
 		}
-		
 	}
-
-
 
 	private static void tableauSemArtificial() {
 		tableau = new Float[numeroDeLinhasTableau][numeroDeColunasTableau];
@@ -125,7 +149,6 @@ public class Main
 			}
 		}
 		
-		
 		for(int i = 0; i < quantidadeVariaveisNaturais; i++) {
 			if(tipoProblema.equals("max")) {
 				tableau[0][i+1] = funcaoObjetivo[i];
@@ -133,7 +156,6 @@ public class Main
 			else if(tipoProblema.equals("min")) 
 				tableau[0][i+1] = funcaoObjetivo[i]*(-1);
 		}
-		
 				
 		for(int i = 1; i < numeroDeLinhasTableau; i++) {
 			for(int j = 1; j < numeroDeColunasTableau; j++) {
@@ -148,8 +170,6 @@ public class Main
 			tableau[i+1][quantidadeVariaveisNaturais] = Float.valueOf(restricoes[i][quantidadeVariaveisNaturais-1]);
 			tableau[i+1][0] = Float.valueOf(restricoes[i][quantidadeVariaveisNaturais+1]);
 		}
-			
-		
 		
 		int posicaoDaFolga = quantidadeVariaveisNaturais+1;
 		int posicaoDaArtificial = numeroDeColunasTableau - variaveisArtificiais;
@@ -164,60 +184,5 @@ public class Main
 				tableau[i+1][posicaoDaArtificial++] = (float) 1;
 			}
 		}
-		
-		
-	}
-
-
-
-	private static void lerEntradas()
-	{
-		try 
-		{
-			//File arquivo = new File(nomeArquivo);
-			//FileReader leitorArquivo = new FileReader(arquivo);
-			//BufferedReader bufferLeitor = new BufferedReader(leitorArquivo);
-			Scanner scanner = new Scanner(System.in);
-			
-			tipoProblema = scanner.nextLine();//bufferLeitor.readLine();
-			String dimensoes[] = scanner.nextLine().split(" ");//bufferLeitor.readLine().split(" ");
-			quantidadeRestricoes = Integer.parseInt(dimensoes[0]);
-			quantidadeVariaveisNaturais = Integer.parseInt(dimensoes[1]);
-			restricoes = new String[quantidadeRestricoes][quantidadeVariaveisNaturais+2];
-			funcaoObjetivo = new float[quantidadeVariaveisNaturais];
-			
-			System.err.println(tipoProblema + " \n" + quantidadeRestricoes + " " + quantidadeVariaveisNaturais);
-			
-			for(int i = 0; i < quantidadeVariaveisNaturais; i++) {
-				funcaoObjetivo[i] = scanner.nextFloat();
-				System.out.print(funcaoObjetivo[i]+"   ");
-			}
-			System.out.println();
-			
-			//Scanner aleatório porque ele ta pegando uma linha vazia sabe-se la de onde
-			scanner.nextLine();
-			
-			for(int i = 0; i < quantidadeRestricoes; i++) {
-				restricoes[i] = scanner.nextLine().split(" ");
-			}
-			
-			scanner.close();
-					
-			//leitorArquivo.close();
-		} 
-		catch(/*IO*/Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	private static void imprimirTableau()
-	{
-		for(int i = 0; i < numeroDeLinhasTableau; i++) {
-			for(int j = 0; j < numeroDeColunasTableau; j++) {
-				System.out.print(tableau[i][j]+"\t");
-			}
-			System.out.println();
-		}		
-	}
+	}	
 }
